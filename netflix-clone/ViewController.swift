c76f372527437405076c56fe2403c223b1f82870
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-09-06 20:04:19 +0300

    Initial commit.

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by Alpsu Dilbilir on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Netflix"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
